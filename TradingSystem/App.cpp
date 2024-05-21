0fc0f782d28551a87a8997d0f0ff780c7331f9a6
KimYoonjae <rladbswo1124@gmail.com>
2024-05-21 14:52:57 +0900

    Add feature/selectStockBrocker

--- TradingSystem/App.cpp
+++ TradingSystem/App.cpp
@@ -1,4 +1,28 @@
-﻿class App {
-public:
+﻿#include "StockerBrocker.cpp"
+#include "KiwerDriver.cpp"
+#include "NemoDriver.cpp"
 
+class App {
+public:
+	void selectStockBrocker(string selectedBrocker)
+	{
+		if (selectedBrocker == "KI")
+		{
+			KiwerDriver kd;
+			stockerbrocker = &kd;
+			cout << "KI 증권사를 선택하셨습니다." << endl;
+		}else if (selectedBrocker == "NE")
+		{
+			NemoDriver nd;
+			stockerbrocker = &nd;
+			cout << "NE 증권사를 선택하셨습니다." << endl;
+		}
+		else
+		{
+			throw exception("증권사는 'KI' 또는 'NE' 중 선택해야 합니다.");
+		}
+	}
+
+protected:
+	StockerBrocker* stockerbrocker;
 };
