60da73ad3a992eee12fb1c615f97d98e1c88f0c2
kicheolha <kcha2299@gmail.com>
2024-05-21 15:57:37 +0900

    byNiceTiming - Refactoring

--- TradingSystem/App.cpp
+++ TradingSystem/App.cpp
@@ -26,18 +26,27 @@ public:
 		return 0;
 	}
 
-	void buyNiceTiming(string stockCode, int price) {
-		int previousPrice[3] = { 0, };
-
+	void updatePreviousPriceForBuy(int  previousPrice[3], std::string& stockCode)
+	{
 		for (int iter = 0; iter < 3; iter++) {
 			previousPrice[iter] = m_stockerBroker->getMarketPrice(stockCode, 1);
 		}
+	}
 
+	void trackRisingPrice(int  previousPrice[3])
+	{
 		for (int iter = 0; iter < 2; iter++) {
 			if (previousPrice[iter] < previousPrice[iter + 1]) continue;
 
 			throw exception("가격 하락세로 보여 buyNiceTiming에 실패합니다.");
 		}
+	}
+
+	void buyNiceTiming(string stockCode, int price) {
+		int previousPrice[3] = { 0, };
+
+		updatePreviousPriceForBuy(previousPrice, stockCode);
+		trackRisingPrice(previousPrice);
 
 		int currentPrice = m_stockerBroker->getMarketPrice(stockCode, 1);
 		int stockCount = (price / m_stockerBroker->getMarketPrice(stockCode, 1));
